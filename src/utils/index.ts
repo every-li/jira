4a52c23d9cf6848a0d2e3f4d7e739014ba18dca2
lzz <1841502391@qq.com>
2021-07-29 09:46:14 +0800

    登录

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,16 +1,33 @@
 import { useEffect, useState } from 'react';
 
 // 判断输入的值是否为false
-export const isFalse = (value: unknown): boolean => {
-  return value === 0 ? false : !value;
-};
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
+
+export const isVoid = (value: unknown) =>
+  value === undefined || value === null || value === "";
 
 // 去除对象中的空值
-export const cleanObject = (object: object) => {
+// export const cleanObject = (object?: { [key: string]:  })unknown => {
+//   const result = { ...object };
+//   Object.keys(result).forEach(key => {
+//     const value = result[key];
+//     if (isFalse(value)) {
+//       delete result[key];
+//     }
+//   });
+//   return result;
+// };
+
+// 在一个函数里，改变传入的对象本身是不好的
+export const cleanObject = (object?: { [key: string]: unknown }) => {
+  // Object.assign({}, object)
+  if (!object) {
+    return {};
+  }
   const result = { ...object };
-  Object.keys(result).forEach(key => {
+  Object.keys(result).forEach((key) => {
     const value = result[key];
-    if (isFalse(value)) {
+    if (isVoid(value)) {
       delete result[key];
     }
   });
